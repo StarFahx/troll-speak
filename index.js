3ebc56ed3e1f28f7121af98180328949fd29a7c0
Robert Brignull <robertbrignull@gmail.com>
2020-06-27 10:41:34 +0100

    add output area and demo content

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('#convert').on('click', function() {
+    $('#output-area').css('color', '#008282');
+    $('#output-troll-name').text('gallowsCalibrator [GC]');
+    $('#output-troll-content').html('GC: 1 R3S3NT TH3 1MPL1C4T1ON D4V3<br/>GC: TH4T 1 4M NOT WORK1NG T1R3L3SSLY ON YOUR B3H4LF 4S W3LL 4S ON B3H4LF OF TH3 F1N3 4RTS<br/>GC: JUST B3C4US3 YOU GO FOR HOURS W1THOUT H34R1NG FROM M3<br/>GC: DO3SNT M34N 1 4M NOT SL4V1NG 4W4Y H3R3 4T M4K1NG YOU R1CH<br/>GC: F1N4NC14LLY 4ND 4RT1ST1C4LLY<br/>GC: 1TS JUST 1 DONT H4V3 TH3 LUXURY OF YOUR 3XP4ND3D T1M3FR4M3<br/>GC: M1ST3R THR33 D4YS 1N ON3 >:[');
+
+    $('#input-area').hide();
+    $('#output-area').show();
+  });
+});
